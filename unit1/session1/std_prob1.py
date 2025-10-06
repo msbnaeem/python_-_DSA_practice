9502c930a7c1f99ad256594f510c3604f95255f5
Mohammad Naeem <binnaeem96@gmail.com>
2025-10-05 22:37:20 -0400

    changes

---
+++ unit1/session1/std_prob1.py
@@ -0,0 +1,21 @@
+# 1 hundred acre wood
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+# welcome()
+
+# 2 greetings
+
+def greetings(name):
+    print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin.")
+
+# greetings('Mohammad')
+
+# 3 catchcephrase
+def print_catchphrase(character):
+
+    phrases = {"Pooh":"Oh brother!", "Trigger":"TTFN: Ta-ta for now!", "Eeyore": "Thanks for noticing me.", 
+               "Christopher Robin": "Silly old bear."}
+    
+    for prase in phrases
+
