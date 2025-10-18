222ccd6a0bb41a4f14b4d55f1b138ab2bf24dadf
msbnaeem <34749667+msbnaeem@users.noreply.github.com>
2025-10-17 22:37:25 -0700

    divisor

--- unit1/session1/std_prob1.py
+++ unit1/session1/std_prob1.py
@@ -116,17 +116,30 @@ def can_pair(item_quantities):
         else:
             return True
 
-
-
-    
-
-
 '''
 Problem 10: Split Haycorns
 Piglet's has collected a big pile of his favorite food, haycorns, and wants to split them evenly amongst his friends. 
 Write a function split_haycorns() to help Piglet determine the number of ways he can split his haycorns into even groups. 
 split_haycorns() accepts a positive integer quantity as a parameter and returns a list of all divisors of quantity.
 '''
+
+def split_haycorns(1, quantity+1):
+    divisors = []
+    if quantity < 0:
+        return "Please Enter positive integer"
+
+    for num in range(quantity):
+        if quantity % num == 0:
+            divisors,append(num)
+    return divisors
+
+
+
+
+
+
+
+
 def split_haycorns(quantity):
      splitted = []
      for i in quantity:
